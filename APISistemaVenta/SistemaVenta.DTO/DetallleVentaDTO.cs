bd5fd46012f5af4aea88803d4f23468f6cc81b3a
Jair <i1921197@continental.edu.pe>
2023-11-15 14:16:57 -0500

    Agregando mi archivo al repositorio

---
+++ APISistemaVenta/SistemaVenta.DTO/DetallleVentaDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.DTO
+{
+    public class DetalleVentaTDO
+    {
+        public int? IdProducto { get; set; }
+        public string? DescripcionProducto { get; set; }
+        public int? Cantidad { get; set; }
+        public string? Precio { get; set; }
+        public string? Total { get; set; }
+        public string? PrecioTexto { get; set; }
+        public string? TotalTexto { get; set; }
+    }
+}
---
+++ APISistemaVenta/SistemaVenta.Utility/DetalleVentaDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SistemaVenta.Utility
+{
+    internal class DetalleVentaDTO
+    {
+        public string? PrecioTexto { get; set; }
+        public string? TotalTexto { get; set; }
+    }
+}
